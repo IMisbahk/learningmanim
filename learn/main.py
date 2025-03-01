cbe3881e13f4e862dd9c93cd117cbf45124dbb4e
Misbah Khursheed <m.misbahkhursheed@gmail.com>
2025-03-01 19:10:39 +0530

    test

--- learn/main.py
+++ learn/main.py
@@ -3,7 +3,18 @@ from manim import *
 class Concircle(Scene):
     def construct(self):
         circle = Circle()
-        circle.set_fill(GREEN, opacity=.3)
-        circle.set_stroke(WHITE)
-        self.play(Create(circle))
+        circle.set_fill(RED, opacity=0.5)
+        circle.set_stroke(color=WHITE, width=4)  
+
+        square = Square()
+        square.set_fill(BLUE, opacity=0.5)
+        square.rotate(PI/4)
+
+        self.play(Create(square))
+        self.play(Transform
+                  (
+                      square, circle
+                  ))
         
+
+    
