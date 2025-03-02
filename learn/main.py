352ecb4a73d304621f1d291562cd48a31106cabd
Misbah Khursheed <m.misbahkhursheed@gmail.com>
2025-03-02 20:29:24 +0530

    MorphingShapes

--- learn/main.py
+++ learn/main.py
@@ -1,19 +1,21 @@
 from manim import *
 
-class Concircle(Scene):
+class MorphingShapes(Scene):
     def construct(self):
-        circle = Circle()
-        circle.set_fill(RED, opacity=0.5)
-        circle.set_stroke(color=WHITE, width=4)  
+        square = Square(color=BLUE, fill_opacity=0.6)
+        circle = Circle(color=GREEN, fill_opacity=0.6)
+        star = Star(n=5, color=RED, fill_opacity=0.6)
 
-        square = Square()
-        square.set_fill(BLUE, opacity=0.5)
+        text = Text("Morphing Shapes").scale(0.7).shift(UP*2)
 
-        square.next_to(circle, direction=LEFT, buff=1)
-
-        self.play(Create(circle))
-        self.wait(0.2)
+        
+        self.play(Write(text))
         self.play(Create(square))
+        self.wait(1)
+        self.play(Transform(square, circle))
+        self.wait(1)
+        self.play(Transform(square, star))
+        self.wait(1)
 
-        self.play(square.animate.shift(2*UP))
-        self.play(circle.animate.shift(2*LEFT))
+        self.play(FadeOut(square), FadeOut(text))
+        self.wait()
